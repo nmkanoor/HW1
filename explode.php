d0fa018c76dac0a99f9b2455330a0815e1ccfac3
Nikhil Kanoor <nk352@afsconnect2.njit.edu>
2016-11-19 19:55:14 -0500

    explode

--- explode.php
+++ explode.php
@@ -1,5 +1,11 @@
 <?php
-pizza  = "piece1 piece2 piece3 piece4 piece5 piece6";
+$pizza  = "piece1 piece2 piece3 piece4 piece5 piece6";
 $pieces = explode(" ", $pizza);
 echo $pieces[0];
 echo $pieces[1];
+
+$data = "foo:*:1023:1000::/home/foo:/bin/sh";
+list($user, $pass, $uid, $gid, $gecos, $home, $shell) = explode(":", $data);
+echo $user; // foo
+echo $pass; // *
+?>
